c64119acf540de9d42e834ef1404944233e8c896
maxvyr <maxvid30@gmail.com>
2021-08-19 14:53:28 +0200

    :sparkles: first - commit - scrap price :sparkles:

---
+++ src/main.rs
@@ -0,0 +1,26 @@
+extern crate reqwest;
+extern crate scraper;
+
+
+use scraper::{Html, Selector};
+
+fn main() {
+    let url = "https://www.kubii.fr/cartes-raspberry-pi/2771-nouveau-raspberry-pi-4-modele-b-2gb-0765756931175.html?search_query=Pi4&results=111";
+    println!("Welcome the data I'm going to get is :");
+    scrape_price_data(url);
+}
+
+
+fn scrape_price_data(url: &str) {
+    // cal url
+    let mut req = reqwest::get(url).unwrap();
+    assert!(req.status().is_success());
+
+    let doc_body = Html::parse_document(&req.text().unwrap());
+    let price = Selector::parse(r#"span[itemprop="price"]"#).unwrap();
+
+    for price_rasp in doc_body.select(&price){
+        let price = price_rasp.text().collect::<Vec<_>>();
+        println!("{}", price[0]);
+    }
+}
